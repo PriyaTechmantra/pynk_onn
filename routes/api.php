9115fc5dea45fce7beaf8da04bd52cd71dfde4a9
PriyaTechmantra <priya.m@techmantra.co>
2025-11-05 18:30:15 +0530

    api commit

--- routes/api.php
+++ routes/api.php
@@ -167,7 +167,7 @@ Route::post('product-wise-report-rsm', [ASEController::class, 'productReportRSM'
 Route::get('inactive/ase/report/vp', [ASEController::class, 'inactiveAseListVP']);
 
 Route::get('vp/state/list/{id}', [ASEController::class, 'vpstateList']);
-Route::get('vp/state/area/list/{id}', [ASEController::class, 'vpstateareaList']);
+Route::get('vp/state/area/list', [ASEController::class, 'vpstateareaList']);
 
 Route::post('team-wise-report-vp', [ASEController::class, 'storeReportVP']);
 
