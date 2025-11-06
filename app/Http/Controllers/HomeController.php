8142c56a8d82a823fa4af18135004dff45a60852
PriyaTechmantra <priya.m@techmantra.co>
2025-11-06 14:42:41 +0530

    api commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -172,7 +172,7 @@ class HomeController extends Controller
 
     public function filterReport(Request $request)
     {
-        
+       
         $month=$request->month;
         $year=$request->year;
         $monthYear = $request->get('month_year'); // e.g., "2025-04"
@@ -333,6 +333,61 @@ class HomeController extends Controller
                         // Get all ASE names under this ASM from retailer_list_of_occ table
                         $aseDetails = DB::table('teams')
                             ->whereRaw("FIND_IN_SET(?, vp_id)", [$vp->id])
+                            ->pluck('ase_id');
+                           
+                        // Parse all ASEs from comma-separated format
+                        foreach ($aseDetails as $aseString) {
+                            $ases = explode(',', $aseString);
+                            foreach ($ases as $ase) {
+                                $trimmed = trim($ase);
+                                if (!in_array($trimmed, $uniqueAseList)) {
+                                    $uniqueAseList[] = $trimmed;
+                                }
+                            }
+                        }
+                        
+                        
+                    
+                        // Get user_ids of those ASEs from users table
+                        $aseUsers = Employee::whereIn('id', $uniqueAseList)->pluck('id');
+                        
+                        if ($aseUsers->isEmpty()) {
+                            $totalSales = 0;
+                        } else {
+                            // Get total sales for these ASE user IDs
+                            $totalSales = DB::table('order_products')
+                                ->join('orders', 'orders.id', '=', 'order_products.order_id')
+                                ->whereIn('orders.user_id', $aseUsers)
+                                ->whereBetween('order_products.created_at', [$startDate, $endDate])
+                                ->sum('order_products.qty');
+                        }
+                    
+                        $data[] = [
+                             'id' => $vp->id,
+                            'name' => $vp->name,
+                            'total_sales' => $totalSales,
+                        ];
+                        
+                        usort($data, function ($a, $b) {
+                            return $b['total_sales'] <=> $a['total_sales'];
+                        });
+                    }
+                break;
+
+                case 'distributor':
+               
+
+		    $disUsers = Distributor::where('is_deleted',0)->orderby('name')->get(); // Get all ASM users
+
+                    $data = []; // Final array for ASM with total sales
+                    
+                    foreach ($disUsers as $vp) {
+                       
+                        $uniqueAseList = [];
+                    
+                        // Get all ASE names under this ASM from retailer_list_of_occ table
+                        $aseDetails = DB::table('teams')
+                            ->whereRaw("FIND_IN_SET(?, distributor_id)", [$vp->id])
                             ->pluck('ase_id');
                            
                         // Parse all ASEs from comma-separated format
