8eded38a0c52a3567312746db4170a2d0ac40b43
Puja <puja@techmantra.co>
2025-11-10 17:18:10 +0530

    Change

--- app/Models/RetailerBarcode.php
+++ app/Models/RetailerBarcode.php
@@ -8,18 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class RetailerBarcode extends Model
 {
     use HasFactory;
-    public function distributor() {
-        return $this->belongsTo('App\Models\User', 'distributor_id', 'id');
-    }
-    public function state() {
-        return $this->belongsTo('App\Models\State', 'state_id', 'id');
-    }
-    
-     public function user() {
-        return $this->belongsTo('App\Models\User', 'member_id', 'id');
-        
-    }
-     public function stock() {
-        return $this->hasMany('App\Models\WarehouseStock', 'qrcode_id', 'id');
-    }
+   
 }
