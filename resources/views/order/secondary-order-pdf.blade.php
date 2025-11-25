0675fae440490491ca432a111688415ab6f03a69
PriyaTechmantra <priya.m@techmantra.co>
2025-11-25 12:47:30 +0530

    api commit

--- resources/views/order/secondary-order-pdf.blade.php
+++ resources/views/order/secondary-order-pdf.blade.php
@@ -187,11 +187,16 @@
 @endsection
 
 @section('script')
-<script src="{{ asset('admin/js/printThis.js') }}"></script>
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://jasonday.github.io/printThis/printThis.js"></script>
 
-<script>
-    function printInvoice() {
-        $('.printDiv').printThis();
-    }
-</script>
+    <script>
+        function printDiv() {
+            $('#DivIdToPrint').printThis({
+                importCSS: true,
+                importStyle: true,
+            });
+        }
+    </script>
 @endsection
