9dd150932ed17a4f05d21d95b5487ee8e94bf0cc
PriyaTechmantra <priya.m@techmantra.co>
2025-11-27 17:58:32 +0530

    api commit

--- resources/views/terms/index.blade.php
+++ resources/views/terms/index.blade.php
@@ -71,7 +71,6 @@
                                         
                                         <div class="form-group">
                                             <button type="submit" class="btn btn-submit">Update</button>
-                                            
                                         </div>
                                     </form>
                                 @else
