3f50d872176811e7d376e5de729e4bb0f9484a3b
PriyaTechmantra <priya.m@techmantra.co>
2025-12-19 17:15:20 +0530

    Apply local changes

--- resources/views/reward/barcode/redeem.blade.php
+++ resources/views/reward/barcode/redeem.blade.php
@@ -65,7 +65,7 @@
                 <div class="card data-card mt-3">
                     <div class="card-header">
                         <h4>QRCODE REDEEM HISTORY
-                            <br>
+                            <br><br>
                             @can('qrcode redeem history export')
                             <a href="{{ url('reward/qrcode/redeem/list/csv/export',['brand'=>$request->brand,'date_from'=>$request->date_from,'date_to'=>$request->date_to,'distributor'=>$request->distributor,'ase'=>$request->ase,'keyword'=>$request->keyword]) }}" class="btn btn-sm btn-cta ms-auto" data-bs-toggle="tooltip" title="Export data in CSV">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
