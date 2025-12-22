c69f6bd96c9d082091fff9d8ea3e83894b00551c
PriyaTechmantra <priya.m@techmantra.co>
2025-12-22 18:10:17 +0530

    Apply local changes

--- app/Http/Controllers/Api/ASEController.php
+++ app/Http/Controllers/Api/ASEController.php
@@ -2065,11 +2065,11 @@ public function aseSalesreport(Request $request)
             $q->where('user_type', $userType)
 
               // CSV match: "1,2,3"
-              ->orWhereRaw("FIND_IN_SET(?, user_type)", [$userType]);
+              ->orWhereRaw("FIND_IN_SET('?', user_type)", [$userType]);
         })
         ->orderByDesc('id')
         ->get();
-        dd(DB::getQueryLog());
+        //dd(DB::getQueryLog());
         if ($data->isNotEmpty()) {
             $data = $data->map(function ($news) use ($brandMap) {
                 // If 'brand' is comma separated (like "1,2"), map multiple names
